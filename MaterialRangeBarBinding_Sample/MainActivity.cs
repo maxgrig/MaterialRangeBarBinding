15fec040f1968234ccda490cbaa48a43eeb0deb8
Max Grigoriev <themaxgrig@gmail.com>
2015-04-02 15:46:41 +0300

    Added Sample

---
+++ MaterialRangeBarBinding_Sample/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MaterialRangeBarBinding_Sample
+{
+	[Activity (Label = "MaterialRangeBarBinding_Sample", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+
+			RangeBarBinding.RangeBar bar = new RangeBarBinding.RangeBar (this);
+
+
+		}
+	}
+}
+
+
